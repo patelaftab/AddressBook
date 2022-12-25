81237fd67c29031d0608229ba3da4d687574f03a
patelaftab <10patelaftab@gmail.com>
2022-12-25 22:48:50 +0530

    [Aftab] Welcome Message

---
+++ AddressBookPro/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AddressBook
+{
+    internal class AddressBook
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Address Book Program!\n");
+        }
+        public AddressBook() { }
+        public static void Main(string[] args)
+        {
+            WelcomeMessage();
+        }
+    }
+}
