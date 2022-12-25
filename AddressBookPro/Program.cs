686fe45717465d2b969b773a25d67e72886ac294
patelaftab <10patelaftab@gmail.com>
2022-12-25 22:52:28 +0530

    [Aftab] Creating a contact With New Person

--- AddressBookPro/Program.cs
+++ AddressBookPro/Program.cs
@@ -6,10 +6,24 @@
         {
             Console.WriteLine("Welcome to Address Book Program!\n");
         }
-        public AddressBook() { }
+        public static void CreatingContact()
+        {
+            Console.WriteLine("Enter your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your Phone_Number");
+            double phoneNo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your Email_Id");
+            string emailId = Console.ReadLine();
+            Console.WriteLine($"\n First Name : {firstName}\n Last Name : {lastName}\n Address : {address}\n Phone Number : {phoneNo}\n Email ID : {emailId}");
+        }
         public static void Main(string[] args)
         {
             WelcomeMessage();
+            CreatingContact();
         }
     }
 }
